3034357b96a184b165b2412d314e4fec4b2721bf
marlonvallecillo <mr_vallecillot@unicah.edu>
2023-02-01 22:04:01 -0600

    CRUD

--- src/libs/Empresas/Empresas.ts
+++ src/libs/Empresas/Empresas.ts
@@ -5,37 +5,57 @@ export interface IEmpresa {
     created?: Date;
     updated?: Date;
     observacion?: string;
-}
-
-
-export class Empresas {
-
+  }
+  
+  
+  export class Empresas {
+  
     private empresas : IEmpresa[];
     constructor(){
-        this.empresas = [];
-    }
-    add(nuevaEmpresa : IEmpresa) {
-        const date = new Date();
-        const nueva: IEmpresa = {
-            ...nuevaEmpresa,
-            código: (Math.random()* 1000).toString() + new Date().getTime().toString(),
-            created: date,
-            updated: date
-        }
-        this.empresas.push(nueva);
-        return true;
+      this.empresas = [];
     }
     getAll(){
-        return this.empresas;
+      return this.empresas;
     }
+    getById(código: string){
+      const empresaToReturn = this.empresas.find((emp)=>{
+        return emp.código === código;
+      });
+      return empresaToReturn;
+    }
+    add(nuevaEmpresa : IEmpresa) {
+      const date = new Date();
+      const nueva: IEmpresa = {
+        ...nuevaEmpresa,
+        código: (Math.random()* 1000).toString()+new Date().getTime().toString(),
+        created: date,
+        updated: date
+      }
+      this.empresas.push(nueva);
+      return true;
+    }
+  
     update(updateEmpresa: IEmpresa){
-        const newEmpresas: IEmpresa[] = this.empresas.map((emp)=>{
-            if( emp.código === updateEmpresa.código) {
-                return {...emp, ...updateEmpresa, updated: new Date()};
-            }
-            return emp;
+      const newEmpresas: IEmpresa[] = this.empresas.map((emp)=>{
+        if ( emp.código === updateEmpresa.código ) {
+          return {...emp, ...updateEmpresa, updated: new Date()};
+        }
+        return emp;
+      });
+      this.empresas = newEmpresas;
+      return true;
+    }
+    delete(código: string){
+      const empresaToDelete = this.empresas.find((emp)=>{
+        return emp.código === código;
+      });
+      if(empresaToDelete){
+        const newEmpresas: IEmpresa[] = this.empresas.filter((emp)=>{
+          return emp.código !== código;
         });
         this.empresas = newEmpresas;
         return true;
+      }
+      return false;
     }
-}
+  }
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -4,33 +4,32 @@ const router  = express.Router();
 
 import empresasRouter from './empresas/empresas';
 // REST API
-// Internet -> HTTP -> REST API -> DB
+// Internet  ->  HTTP  ->  REST API JSON ->  DB
 // SOAP XML wsdl
 // {} -> JSON
 // [] -> JSON
 // { llave : valor }
 // valor: texto, numérico, booleano, array [valores], objeto {llave:valor}
 
-// REST: stateless, resource unique representation
-// CRUD: Create, Read, Update, Delete
-//       POST, GET, PUT, DELETE  (Para Create usamos POST, Read usamos GET, Update usamos Put, eliminado usamos Delete)
+// REST stateless, resource unique representation
+// CRUD Create, Read, Update, Delete
+//      POST, GET, PUT, DELETE
 
-//http://localhost:3001
+// http://localhost:3001
 router.get('/', (_req, res) => {
   res.json({msg:'Hello World!'});
  });
 
- //http://localhost:3001/version
+// http://localhost:3001/version
 router.get('/version', (_req, res)=>{
   const version: string = "1.0.0";
   const jsonResp = {"name":"FODA Be", "version": version};
-  //string, number, boolean, types, interfaces, classes, enumerators
-  res.json(jsonResp)
-});
+  // string, number, boolean, types, interfaces, classes, enumerators
+  res.json(jsonResp);
+ });
 
-//http://localhost:3001/empresas
-router.use('/empresas', empresasRouter)
+router.use('/empresas', empresasRouter);
 
-//router.get router.post router.put router.delete router.use
+ //router.get  router.post router.put router.delete  router.use
 
 export default router;
