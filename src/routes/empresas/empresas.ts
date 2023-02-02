a478f44901387c56d006adb91f09043277cb99ed
marlonvallecillo <mr_vallecillot@unicah.edu>
2023-02-01 21:41:49 -0600

    CRUD

--- src/routes/empresas/empresas.ts
+++ src/routes/empresas/empresas.ts
@@ -1,9 +1,10 @@
 import express from 'express';
 const router = express.Router();
 
-import { Empresas} from '@libs/Empresas/Empresas';
+import { Empresas, IEmpresa} from '@libs/Empresas/Empresas';
 
 const empresasModel = new Empresas();
+
 empresasModel.add({
     código: '',
     nombre: 'Mi empresa',
@@ -22,14 +23,62 @@ router.get('/', (_req, res)=>{
     res.status(200).json(jsonUrls)
 })
 
-//http://localhost:3001/empresas/all
 router.get('/all', (_req, res)=>{
     res.status(200).json(empresasModel.getAll());
+});
+
+router.post('/new', (req, res) => {
+    console.log("Empresas /new request body:", req.body)
+    const {
+        nombre="Jonh Doe Corp", 
+        status="Activo"
+    } = req.body;
+    //TODO: Validar Entrada de datos
+    const newEmpresa: IEmpresa = {
+        código : "",
+        nombre,
+        status
+    };
+    if (empresasModel.add(newEmpresa)) {
+        res.status(200).json({"created": true});
+    }
+    return res.status(404).json(
+        {"error":"Error al agregar una nueva Empresa"}
+        );
+});
+
+router.put('/upd/:id', (req, res) => {
+    const { id } = req.params;
+    const {
+        nombre = "Jonh Doe Corp",
+        status = "Activo",
+        observacion = ""
+    } = req.body;
+
+    const UpdateEmpresa : IEmpresa = {
+        código: id,
+        nombre,
+        status,
+        observacion
+    };
+
+    if (empresasModel.update(UpdateEmpresa)){
+        return res
+        .status(200)
+        .json({"update": true});
+    }
+    return res
+    .status(404)
+    .json(
+        {
+            "error": "Error al actualizar Empresa"
+        }
+    );
 });
 /*
 declarar funciones se conoce como fecha gorda (ad arrow)
 router.get('/', function(_req, res)=>{
-
 })
 */
+
 export default router;
