c09136657c0640dab4a95af2d1eda7782bad31bc
marlonvallecillo <mr_vallecillot@unicah.edu>
2023-03-08 19:56:42 -0600

    new date

--- src/libs/Users/Users.ts
+++ src/libs/Users/Users.ts
@@ -1,32 +1,38 @@
-import { IUser } from "@dao/models/Users/IUsers";
 import { IDataAccessObject } from "@dao/IDataAccessObject";
-export class Usuarios {
-  private dao: IDataAccessObject;
-  constructor(dao: IDataAccessObject) {
-    this.dao = dao;
+import { UserDao } from "@dao/models/Users/UserDao";
+import { Security } from "@utils/Security";
+
+export class Users {
+  private userDao: UserDao;
+  constructor(user: IDataAccessObject) {
+    this.userDao = user as UserDao;
   }
-  getAll() {
-    return this.dao.findAll();
-  }
-  getById(id: string) { 
-    return this.dao.findByID(id);
-  }
-  add(nuevousuario: IUser) {
-    const date = new Date();
-    const nueva: IUser = {
-      ...nuevousuario,
-      created: date,
-      updated: date
+  public async newUser(email: string, password: string) {
+    try {
+      const newUser = {
+        email,
+        password: Security.encodePassword(password),
+        pswdExpires: new Date(new Date().getTime()+(3 * 30 * 24 * 60 * 60 * 1000))
+      };
+      const result = await this.userDao.create(newUser);
+      const rt = await this.userDao.findOneByFilter({ _id: result?.insertedId });
+      delete rt.password;
+      return rt;
+    } catch (ex) {
+      console.error('newFoda error:', ex);
+      return null;
     }
-    return this.dao.create(nueva);
   }
-
-  update(id: string, updateUsuario: IUser) {
-    const updateObject = { ...updateUsuario, updated: new Date() };
-    return this.dao.update(id, updateObject);
-  }
-
-  delete(id: string) {
-    return this.dao.delete(id);
+  public async loginUser(email:string, password:string) {
+    try{
+      const dbUser = await this.userDao.findOneByFilter({email});
+      if (Security.verifyPassword(password, dbUser.password)){
+        delete dbUser.password;
+        // JWT
+      }
+    }catch(err){
+      console.error(err);
+      throw new Error("Can´t Validate Credentials");
+    }
   }
 }
